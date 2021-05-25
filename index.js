c675f3ee6bddc741a49f994b2dfff0414e962add
zearkiatos <caprilespe@outlook.com>
2021-05-24 20:18:14 -0400

    Install express and configuration application

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+const express = require('express');
+const api = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
 const router = jsonServer.router('src/db.json');
 const middlewares = jsonServer.defaults();
 const { config } = require('./config');
 
-server.use(middlewares);
-server.use(router);
-server.use((req, res, next) => {
+require('http').Server(api);
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(cors());
+api.use(morgan('tiny'));
+api.use(helmet());
+
+api.use(middlewares);
+api.use(router);
+api.use((req, res, next) => {
  if (isAuthorized(req)) {
    next()
  } else {
    res.sendStatus(401)
  }
 });
-server.listen(config.PORT, () => {
+api.listen(config.PORT, () => {
   console.log('JSON Server is running 🤖')
 });
